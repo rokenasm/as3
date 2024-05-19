9ce374c8fe772782ed0855f50d7b7a6cf5759ea9
Rokas <rokenasm@gmail.com>
2024-05-19 13:03:20 +0100

    enemy collision working

--- script.js
+++ script.js
@@ -77,12 +77,19 @@ function keyDown(event) {
 
 const player = document.querySelector('#player');
 const playerMouth = player.querySelector('.mouth');
+const enemy = document.querySelector('enemy');
 let playerTop = 0;
 let playerLeft = 0;
+let enemyTop = 0;
+let enemyBotttom = 0;
+
+
+
+
 
 
 /* COLLISION WALLS COMPLETED*/
-setInterval(function() {
+  setInterval(function() {
     pointCheck();
     if (downPressed == true) {
 
@@ -92,13 +99,14 @@ setInterval(function() {
         let btmR = document.elementFromPoint(position.right, newBottom);
 
         if (btmL.classList.contains('wall')== false && btmR.classList.contains('wall') == false) {
+            if (btmL.classList.contains('enemy')== false && btmR.classList.contains('enemy') == false) {
             playerTop++;
             player.style.top = playerTop + 'px';     
         }
     
           playerMouth.classList='down';
     }
-    
+}
 
     
     if(upPressed == true) {
@@ -109,14 +117,15 @@ setInterval(function() {
         let topL = document.elementFromPoint(position.left, newTop);
         let topR = document.elementFromPoint(position.right, newTop);
 
-        if (topL.classList.contains('wall')== false && topR.classList.contains('wall') == false) {
+        if (topL.classList.contains('wall')== false && topR.classList.contains('wall') == false) { 
+         if (topL.classList.contains('enemy') == false && topR.classList.contains('enemy') == false) {
             playerTop--;
             player.style.top = playerTop + 'px';
 
         }
         playerMouth.classList = 'up';
     }
-
+    }
        
 
 
@@ -127,12 +136,15 @@ setInterval(function() {
         let btmL = document.elementFromPoint (newLeft, position.bottom);
 
         if(topL.classList.contains('wall ') == false && btmL.classList.contains('wall') == false) {
+            if(topL.classList.contains('enemy') == false && btmL.classList.contains('enemy') == false) {
              playerLeft--;
             player.style.left =playerLeft + 'px'
         }
         
         playerMouth.classList = 'left';
         }
+    }
+
 
     if(rightPressed == true) {
         let position = player.getBoundingClientRect()
@@ -141,21 +153,32 @@ setInterval(function() {
         let btmR = document.elementFromPoint(newRight, position.bottom)
         
         if (topR.classList.contains('wall')== false && btmR.classList.contains('wall')==false) {
+            if (topR.classList.contains('enemy')== false && btmR.classList.contains('enemy')==false) {
         playerLeft++;
         player.style.left = playerLeft + 'px';
         
         }
       playerMouth.classList = 'right';
     }
+}
     
 }, 10);
 
+/*  POINTS COMPLETED /*    */
+/* Game over doesnt work 
+function gameOver(){
+    let h1tag = document.querySelector('h1');
+    h1tag.firstChild.nodeValue= 'Game over!';
+    start.style.display= 'flex';
+    clearInterval(timer);
+}
+  */
 
-/*  POINTS COMPLETED */
 
 function pointCheck() {
     const position = player.getBoundingClientRect();
     const points = document.querySelectorAll('.point');
+    let num = points.length;
 
     for (let i = 0; i < points.length; i++) {
         let pos = points[i].getBoundingClientRect();
@@ -167,24 +190,30 @@ function pointCheck() {
 
         ) {
             points[i].classList.remove('point');
+            let elements = document.querySelector('p')
+            elements.firstChild.nodeValue++;
+            num--;
+            if(num ==0) {
+                Gameover();
+            }
+
         }
     }
 
 }
-    
-
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
 
 
 
+ /* TITLE */   
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.getElementById('start1');
+    startButton.addEventListener('click', () => {
+        startButton.style.display = 'none';
+        document.addEventListener('keydown', keyDown);
+        document.addEventListener('keyup', keyUp);
+    });
+});
 
-/* TITLE COMPLETED */
-let start1 = document.querySelector('#start1');
-
-start1.onclick = function(){
-    start1.parentNode.removeChild(start1);
-}
 
 /* COLOURS */
 
@@ -195,23 +224,18 @@ for (let i =0 ; i < colors.length; i++) {
     colors[i].addEventListener('click', setColor);
 }
 
+
+
+
 function setColor() {
     player.style.backgroundColor = this.id;
 }
 
 
-/* CLOSE SIDEBAR */
 
-function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    const main = document.getElementById("main");
 
-    if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-250px";
-        main.style.marginLeft = "0";
-    } else {
-        sidebar.style.left = "0px";
-        main.style.marginLeft = "250px";
-    }
-}
+
+
+
+
 
